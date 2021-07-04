a0f9b5a8e79f262d86a3d12b4386354d1a9c7e3d
Ahmed <koshio@AhmedMacBook-Air.local>
2021-07-04 14:38:01 +0900

    created mvvm ui
    Committer: Ahmed <einsteinium25@gmail.com>

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,115 +10,39 @@ import SwiftUI
 struct ContentView: View {
     //var emojis: Array<String> = ["🤣","😜","🤩"]
     //var emojis: [String] = ["🤣","😜","🤩"]
-    var emojis = ["🤣","😜","🤩","😀","😇","😅","😍","😘","🥰","😤","😡","🤬","🤯","🤗","🤔","🤭","🤫","🤥","😲"]
-    @State var emojiCount = 3
-    var body: some View {
-        VStack{
-//            HStack {
-//            LazyVGrid(columns: [GridItem(),GridItem(),GridItem()] ) {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))] ) {
-                    //CardView(isFaceUp: false)
-                    ForEach(emojis[0..<emojiCount], id:\.self) { emoji in
-                        CardView(content: emoji).aspectRatio(2/3,contentMode: .fit)
-                    }
-        //            CardView(content: "🤣")
-        //            CardView(content: "😜")
-        //            CardView(content: "🤩")
-                    //CardView(isFaceUp: false)
-                }
-            }.foregroundColor(.red)
-        
-            Spacer(minLength: 12)
-            HStack {
-//                Button(action: {
-//                        emojiCount += 1
-//                } , label: {
-//                    VStack{
-//                        Text("Add")
-//                        Text("Card")
-//                    }
-//
-//                })
-                remove
-                Spacer()
-                add
-            }
-            .font(.largeTitle)
-            .padding(.horizontal)
-            
-
-        }
-        .padding(.horizontal)
-        
-    }
+//    var emojis = ["🤣","😜","🤩","😀","😇","😅","😍","😘","🥰","😤","😡","🤬","🤯","🤗","🤔","🤭","🤫","🤥","😲"]
+//    @State var emojiCount = 3
     
-    var remove: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
+    //@ObservedObject let viewModel: EmojiMemoryGame
+    //Property wrapper can only be applied to a 'var'
+    @ObservedObject var viewModel: EmojiMemoryGame
+    var body: some View {
+//        VStack{
+
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))] ) {
+                //CardView(isFaceUp: false)
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3,contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
+
             }
-        } label: {
-            Image(systemName: "minus.circle")
-//           VStack{
-//                Text("Remove")
-//                Text("Card")
-                
-//            }
-            
-        }
-        
-    }
- 
-    var add: some View {
-//        Button(action: {
-        Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
-            }
-        } label: {
-            Image(systemName: "plus.circle")
-//            VStack{
-//                Text("Add")
-//                Text("Card")
-//            }
-            
-        }
+        }.foregroundColor(.red)
+//        }
     }
 }
     
-//        return ZStack {
-//            RoundedRectangle(cornerRadius: 20.0)
-//                .stroke(lineWidth: 3.0)
-////                .padding(.horizontal)
-////                .foregroundColor(.red)
-//
-//            Text("Hello, world!")
-////                .foregroundColor(Color.orange)
-////                .padding()
-//        }
-//        .padding(.horizontal)
-//        .foregroundColor(.red)
-//
-////        Text("Hello, world!")
-////            .foregroundColor(Color.orange)
-////            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-//
-////        RoundedRectangle(cornerRadius: 20.0).stroke(lineWidth: 3.0).padding(.horizontal).foregroundColor(.red)
-//    }
-//}
-
     struct CardView: View {
-        //var isFaceUp: Bool { return false }
-        var content: String
-        @State var isFaceUp: Bool = true
-        //var isFaceUp: Bool
-        
+        let card: MemoryGame<String>.Card
         var body: some View{
             ZStack {
-                //var shape = RoundedRectangle(cornerRadius: 20.0)
+                
                 let shape = RoundedRectangle(cornerRadius: 20.0)
-                if isFaceUp {
+                if card.isFaceUp {
                     //RoundedRectangle(cornerRadius: 20.0)
                     shape.fill().foregroundColor(.white)
                     //RoundedRectangle(cornerRadius: 20.0)
@@ -127,27 +51,25 @@ struct ContentView: View {
 
                    
                     //Text("🤣")
-                    Text(content)
+                    Text(card.content)
                         .font(.largeTitle)
                 } else {
                     //RoundedRectangle(cornerRadius: 20.0)
                     shape.fill()
                 }
             }
-            .onTapGesture {
-                isFaceUp = !isFaceUp
-            }
-           //.padding(.horizontal)
-           //.foregroundColor(.red)
+
        }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        let game = EmojiMemoryGame()
+        ContentView(viewModel: game)
             .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
-        ContentView()
+        ContentView(viewModel: game)
             .preferredColorScheme(.light)
     }
 }
--- Memorize/EmojiMemoriGame.swift
+++ Memorize/EmojiMemoriGame.swift
@@ -5,4 +5,55 @@
 //  Created by Ahmed on 2021/07/02.
 //
 
-import Foundation
+import SwiftUI
+
+/*
+func makeCardContent(index: Int)-> String{
+    return "😀"
+}
+*/
+
+//let emojis = ["🤣","😜","🤩","😀","😇","😅","😍","😘","🥰","😤","😡","🤬","🤯","🤗","🤔","🤭","🤫","🤥","😲"]
+
+class EmojiMemoryGame: ObservableObject { //used to publish the chenged model to the world
+    //privat(set) var model: MemoryGame<String>
+    //type variable
+    static let emojis = ["🤣","😜","🤩","😀","😇","😅","😍","😘","🥰","😤","😡","🤬","🤯","🤗","🤔","🤭","🤫","🤥","😲"]
+    
+    //type function
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) {
+            pairIndex  in
+            EmojiMemoryGame.emojis[pairIndex]
+        }
+    }
+    
+//    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    @Published private var model: MemoryGame<String> = createMemoryGame() //can be done only during initialization/ accessing static inside
+    //another static
+    //using var instead of let; because MemoryGame is mutating cards
+    //let would have turned model into immutable
+    
+    
+//    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4) {
+//        pairIndex  in
+//        EmojiMemoryGame.emojis[pairIndex]
+//    }
+    var cards: Array<MemoryGame<String>.Card>{
+        model.cards
+    }
+    
+    //implicit var inside ObservableObject; automatically sends the changed info
+    //var objectWillChange: ObservableObjectPublisher
+    
+    
+    //MARK: - Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card){
+        
+        //objectWillChange.send()//sends model changed info to UI
+        //replaced by @Published keyword for the model
+        model.choose(card)
+    }
+    
+}
